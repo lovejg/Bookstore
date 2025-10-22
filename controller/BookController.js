d9b6516cf03637fe1a0bc0d1700840c66ec7a6d5
tmakdrl <tmakdrl@naver.com>
2025-10-22 16:31:37 +0900

    fifth

--- controller/BookController.js
+++ controller/BookController.js
@@ -3,7 +3,8 @@ const { StatusCodes } = require("http-status-codes");
 
 const allBooks = (req, res) => {
   let { category_id, news, limit, currentPage } = req.query;
-  let sql = "SELECT * FROM books";
+  let sql =
+    "SELECT *, (SELECT count(*) FROM likes WHERE books.id=liked_book_id) AS likes FROM books";
   let offset = limit * (currentPage - 1);
   let values = [category_id];
 
@@ -33,10 +34,12 @@ const allBooks = (req, res) => {
 };
 
 const bookDetail = (req, res) => {
-  let { id } = req.params;
+  let { user_id } = req.body;
+  let { book_id } = req.params.id;
   let sql =
-    "SELECT * FROM books LEFT JOIN category ON books.category_id = category.id WHERE books.id=?";
-  conn.query(sql, id, (err, results) => {
+    "SELECT *, (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes, (SELECT EXISTS (SELECT * FROM likes WHERE user_id=? AND liked_book_id=?)) AS liked FROM books LEFT JOIN category ON books.category_id = category.category_id WHERE books.id=?";
+  let values = [user_id, book_id, book_id];
+  conn.query(sql, values, (err, results) => {
     if (err) {
       return res.status(StatusCodes.BAD_REQUEST).end();
     }
