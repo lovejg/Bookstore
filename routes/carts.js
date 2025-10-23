e7d44de92e9c630d427769df3351bff343ae6b86
tmakdrl <tmakdrl@naver.com>
2025-10-23 12:15:06 +0900

    finish cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const {
+  addToCart,
+  getCartItems,
+  removeCartItem,
+} = require("../controller/CartController");
 
 router.use(express.json());
 
 // 장바구니 담기
-router.post("/", (req, res) => {});
+router.post("/", addToCart);
 
-// 장바구니 조회
-router.get("/", (req, res) => {});
+// 장바구니 조회, 선택한 장바구니 상품 목록 조회
+router.get("/", getCartItems);
 
 // 장바구니 도서 삭제
-router.delete("/:id", (req, res) => {});
-
-// 장바구니에서 선택한 주문 예상 상품 목록 조회(미완)
-router.get("/", (req, res) => {});
+router.delete("/:id", removeCartItem);
 
 module.exports = router;
